8361025b2dfa29022e0dea18d7a5227e9c364951
Rogerio Gomes <rogerio.wcast@gmail.com>
2017-12-09 16:03:25 -0200

    first commit

---
+++ src/QRcoding.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roger
+ * Date: 12/9/17
+ * Time: 4:01 PM
+ */
+
+namespace QRcoding;
+
+include "libs/qrlib.php";
+
+
+class QRcoding
+{
+    public function make(){
+
+
+
+    }
+
+}
